6b96b7e398372a4151932ed89618e44e846267d1
wowling <lingyuhe@tuandai.com>
2018-05-29 18:09:49 +0800

    python base 01

---
+++ PythonBase01.py
@@ -0,0 +1,49 @@
+import math
+
+
+result = 10+29*39-24/8
+
+print(result)
+
+qq=not 1<2;
+
+print(qq)
+
+week=['Monday','Tuesday','Wednesday','Thursday','Friday']
+
+print(len(week))
+
+week.append('Saturday')
+
+print(week)
+
+week.insert(6,'Sunday')
+
+print(week)
+
+list.reverse(week)
+
+print(week)
+
+list.sort(week)
+
+print(week)
+
+week.append(1)
+
+print(week)
+
+list_empty=[None]*10
+
+print(list_empty)
+
+eightJZ = 0o10
+print(eightJZ)
+
+print(math.pi)
+
+print(dir(math))
+
+name = '恭喜'.encode()
+
+print(name)
