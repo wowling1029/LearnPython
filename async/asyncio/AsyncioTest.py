cccb9a90c0795ce16804e821842c6464a9b78fef
wowling <lingyuhe@tuandai.com>
2018-08-12 23:30:51 +0800

    update

--- async/asyncio/AsyncioTest.py
+++ async/asyncio/AsyncioTest.py
@@ -4,21 +4,22 @@ import time
 @asyncio.coroutine
 def sayHello():
     print('Hello World!')
+    # 获取当前时间
     beforeTime = int(time.time())
     timeArray = time.localtime(beforeTime)
     otherStyleTime = time.strftime("%Y--%m--%d %H:%M:%S", timeArray)
     print(otherStyleTime)
     r = yield from asyncio.sleep(2)
     print('Hello again!')
+    # 再次获取当前时间
     nowTime = int(time.time())
     timeArray = time.localtime(nowTime)
     otherStyleTime = time.strftime("%Y--%m--%d %H:%M:%S", timeArray)
     print(otherStyleTime)
+
 
 loop = asyncio.get_event_loop()
 
 loop.run_until_complete(sayHello())
 
 loop.close()
-
-
---
+++ db/MongoDB/MongoDB01.py
@@ -0,0 +1,3 @@
+import pymongo
+
+pymongo.helpers
